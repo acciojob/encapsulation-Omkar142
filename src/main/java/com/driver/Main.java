4ab8dcd95dec69c57ac11647096372741860490b
Omkar142 <omkargochhayat@gmail.com>
2023-02-14 23:56:21 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+
+        RWOnly obj  = new RWOnly();
+
+        //'name' has private access in 'com.driver.RWOnly'
+        //obj.name = "Tushar";
+
+        obj.setName("Rauch");
+
+        System.out.println(obj.getName());
+    }
+
 }
